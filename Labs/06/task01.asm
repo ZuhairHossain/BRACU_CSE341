b1f49eaba280f9cdaafa76ee267203c2239e3258
Syed Zuhair Hossain <szhzuhair@gmail.com>
2021-08-24 02:23:13 +0600

    Add files via upload

---
+++ Labs/06/task01.asm
@@ -0,0 +1,105 @@
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA 
+                               
+n db 12 dup(?) 
+                              
+;variables 
+ 
+    a db "Enter Name: $" 
+    b db "Output: $"
+
+
+    .CODE 
+    MAIN PROC 
+    
+    ;initialize DS
+    
+    MOV AX,@DATA 
+    MOV DS,AX 
+     
+    ; enter your code here  
+    
+    ; TAKING MY NAME AS INPUT
+         
+    lea dx,a
+    mov ah,9
+    int 21h 
+    
+    mov cx,12
+    mov si,0 
+    mov ah,1   
+    
+    start: 
+    int 21h  
+    mov n[si],al
+    add si,1
+    loop start  
+    
+    ;PRINTING MY NAME AS IT WAS WANTED IN THE QUESTION:
+    
+    mov ah,2
+    mov dl,0DH
+    int 21h
+    mov dl,0AH
+    int 21h  
+    
+    lea dx,b
+    mov ah,9
+    int 21h 
+    
+    
+    mov cx,12
+    mov si,0 
+    mov ah,2 
+    
+    loop_start:
+    cmp n[si],41h  
+    jge capital_second_check 
+    
+    cmp n[si],20h
+    je print
+       
+    
+    capital_second_check:
+    cmp n[si],5Ah
+    jle smallaization
+    jmp small_second_check
+    
+    
+    smallaization:
+    add n[si],20h 
+    jmp print 
+    
+    
+    small_second_check:
+    cmp n[si], 7Ah
+    jle capitalization
+    
+    capitalization:
+    sub n[si],20h
+    jmp print
+    
+    print:
+    mov dl,n[si]  
+    mov ah,2
+    int 21h
+    add si,1
+    
+    loop loop_start
+ 
+
+;exit to DOS   
+exit:
+               
+MOV AX,4C00H
+INT 21H 
+
+MAIN ENDP
+    END MAIN 
+  
+
+
+
---
+++ Labs/06/task03.asm
@@ -0,0 +1,110 @@
+.MODEL SMALL
+.STACK 100H
+.DATA 
+
+array db 1,2,3,4,5
+
+;variables 
+                  
+a db "Enter 5 numbers: $" 
+b db "Sorted Order: $ "
+
+.CODE 
+MAIN PROC 
+
+;initialize DS
+
+MOV AX,@DATA 
+MOV DS,AX 
+ 
+    ; enter your code here
+    
+    lea dx,a
+    mov ah,9
+    int 21h    
+    
+    ;taking input
+    
+    mov cx,5
+    mov si,0
+    mov ah,1
+    
+    start_loop:
+    
+    int 21h
+    mov array[si],al
+    add si,1
+    
+    loop start_loop
+    
+    ; sorting the array
+    
+    mov dl,0  
+    
+    nested1:  
+    
+    cmp dl,3
+    jg end
+    
+    mov cx,4
+    mov si,0
+    mov di,1
+    
+    nested2:
+    mov al,array[si]
+    mov bl,array[di]
+    cmp al,bl
+    jg swap   
+    
+    jmp ignore
+                   
+    swap:
+    mov array[si],bl         
+    mov array[di],al
+    
+    ignore:
+    add si,1
+    add di,1 
+    
+    loop nested2
+    
+    add dl,1
+    
+    jmp nested1  
+    
+    end:
+    
+    ; printing
+    
+    mov ah,2
+    mov dl,0DH
+    int 21h
+    mov dl,0AH
+    int 21h
+    
+    lea dx,b
+    mov ah,9
+    int 21h 
+    
+    mov cx,5
+    mov si,0
+    mov ah,2
+    
+    print:
+    mov dl,array[si]
+    int 21h
+    add si,1
+    loop print
+
+
+;exit to DOS 
+               
+MOV AX,4C00H
+INT 21H 
+
+MAIN ENDP
+    END MAIN 
+  
+
+
+
