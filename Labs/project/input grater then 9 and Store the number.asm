8db44ddd8c26f37218d468b23eab3e0abc0aa522
Syed Zuhair Hossain <szhzuhair@gmail.com>
2021-09-10 00:52:06 +0600

    Add files via upload

---
+++ Labs/project/input grater then 9 and Store the number.asm
@@ -0,0 +1,32 @@
+.MODEL SMALL
+.STACK 10H
+.CODE
+
+
+MAIN PROC
+    
+    MOV BH,0
+    MOV BL, 10D
+    
+    INPUT:
+    MOV AH, 1
+    INT 21H
+    CMP AL, 130
+    JNE NUMBER
+    
+    JMP EXIT
+    
+    NUMBER:
+    SUB AL, 30H
+    MOV CL, AL
+    MOV AL, BH
+    MUL BL
+    ADD AL, CL
+    MOV BH, AL
+    
+    JMP INPUT
+    
+    
+    EXIT:
+    MAIN ENDP
+END MAIN
